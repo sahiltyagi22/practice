ac7b63790c8af41e3015e6c0f1ae9a681c65b29c
sahil <sahiltyagi1204@outlook.com>
2023-10-28 17:14:34 +0530

    shortest path in inweighted graph

---
+++ GRAPHS/shortestPath(unweigh).js
@@ -0,0 +1,96 @@
+class Graph {
+    constructor() {
+        this.list = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.list[vertex]) {
+            this.list[vertex] = [];
+        }
+    }
+
+    addEdges(vertex1, vertex2) {
+        if (!this.list[vertex1]) {
+            this.addVertex(vertex1);
+        }
+        if (!this.list[vertex2]) {
+            this.addVertex(vertex2);
+        }
+        this.list[vertex1].push(vertex2);
+        this.list[vertex2].push(vertex1);
+    }
+
+    showConnections() {
+        Object.keys(this.list).forEach((vertex) => {
+            console.log(`${vertex} ==> ${this.list[vertex].join(', ')}`);
+        });
+    }
+
+    DFSr(s, visited) {
+        visited[s] = true;
+        console.log(s);
+
+        let adj = this.list;
+        for (let i = 0; i < adj[s].length; i++) {
+            let u = adj[s][i];
+            if (visited[u] === false) {
+                this.DFSr(u, visited);
+            }
+        }
+    }
+
+    DFS(s) {
+        let visited = {};
+        for (let vertex in this.list) {
+            visited[vertex] = false;
+        }
+
+        this.DFSr(s, visited);
+    }
+
+    shortestPath(s){
+        let visited = new Array().fill(false)
+        let queue = []
+        let distance = []
+        distance[s] = 0
+
+        queue.push(s)
+        visited[s] = true
+
+        while(queue.length >0){
+            let u = queue.shift()
+            this.list[u].forEach(Element =>{
+                if(!visited[Element]){
+                    distance[Element] = distance[u]+1
+                    visited[Element] = true
+                    queue.push(Element)
+                }
+            })
+        }
+
+        return distance
+
+    }
+
+    
+}
+
+
+
+const graph = new Graph()
+graph.addVertex(2)
+graph.addVertex(3)
+graph.addVertex(4)
+graph.addVertex(5)
+graph.addVertex(6)
+graph.addVertex(7)
+
+graph.addEdges(2 , 3)
+graph.addEdges(2 , 4)
+graph.addEdges(2 , 5)
+graph.addEdges(5 , 3)
+graph.addEdges(7 , 3)
+
+
+console.log(graph.shortestPath(2));
+
